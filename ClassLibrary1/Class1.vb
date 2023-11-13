588f50548e84d87e92e4ce416928803c872a02dd
maxim-polyakov <maxim7012@mail.ru>
2023-11-13 21:34:28 +0300

    VBA project

---
+++ ClassLibrary1/Class1.vb
@@ -0,0 +1,11 @@
+Public Class Class1
+    ''Dim wb As Workbook
+    Dim FolderPath As String
+    Dim FilePath As String
+    ''FolderPath = "C:\Users\sumit\OneDrive\Desktop\Project\"
+    ''FilePath = Dir(FolderPath & "*Sales*" & ".xls*")
+    ''Do While FilePath <> ""
+    ''Set wb = Workbooks.Open(FolderPath & FilePath)
+    ''      FilePath = Dir
+    ''Loop
+End Class
